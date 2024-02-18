37600d8ece1c50c4181a1973c7f4a2a949540177
Sabbir45ali <sabbir84b@gmail.com>
2024-02-18 14:06:00 +0530

    All Commited

---
+++ src/Practice of 10 from apna college/funxtionOfCheckGrater.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class funxtionOfCheckGrater {
+  public static int graterCheck(int a,int b) {
+    int ans = a>b?a:b;
+    return ans;
+  }
+  public static void main(String[] args) {
+    Scanner inp = new Scanner(System.in);
+
+    System.out.print("Enter the value of a - ");
+    int a = inp.nextInt();
+
+    System.out.print("Enter the value of b -");
+    int b = inp.nextInt();
+
+    int ans = graterCheck(a, b);
+    System.out.println("Grater among a and b is - "+ans);
+  }
+}
