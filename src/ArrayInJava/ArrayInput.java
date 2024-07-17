4372ea3e0ce62029ad45699894edaa3fcf9794f8
Sabbir45ali <sabbir84b@gmail.com>
2024-07-17 19:29:30 +0530

    Refactor ArrayInput class to close the Scanner object after user input

--- src/ArrayInJava/ArrayInput.java
+++ src/ArrayInJava/ArrayInput.java
@@ -1,11 +1,13 @@
 package ArrayInJava;
 import java.lang.reflect.Array;
 import java.util.*;
+import java.util.*;
 
 public class ArrayInput {
     public static void main(String[] args) {
         int []arr = new int[10];
         Scanner inp = new Scanner(System.in);
+        inp.close();
         System.out.println("Enter 10 numbers - ");
         for(int i=0;i<arr.length;i++) {
             arr[i] = inp.nextInt();
